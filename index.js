e811a755cf42c8ceb4cd59dd81489a4edbfd4652
Murali <murali@appbrowser.com>
2016-11-10 15:38:10 +0530

    Port changed to 5000

--- index.js
+++ index.js
@@ -44,6 +44,6 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 5011, function () {
-  console.log("Listening on port 5011...");
+app.listen(process.env.PORT || 5000, function () {
+  console.log("Listening on port 5000...");
 });
